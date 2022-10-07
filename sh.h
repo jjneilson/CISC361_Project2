4b1ea65ac048d73f7d84c20d38209f257914c9f8
jjneilson <neilson.john38781@gmail.com>
2022-10-07 18:58:26 +0000

    fixed function header for where to be same as actual function

--- sh.h
+++ sh.h
@@ -4,7 +4,7 @@
 extern int pid;
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
-char *where(char *command, struct pathelement *pathlist);
+void where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
 void printenv(char **envp);
 char *prompt(char *prefix, char *args);
