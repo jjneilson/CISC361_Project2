787994c29ef027eec2be91eecccea391097194e9
jjneilson <neilson.john38781@gmail.com>
2022-10-08 20:21:46 +0000

    added sig_handler to ignore ctrl-c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void sig_handler(int signal);
 int main( int argc, char **argv, char **envp )
 {
   /* put signal set up stuff here */
-
+  signal(SIGINT,sig_handler);
   return sh(argc, argv, envp);
 }
 
